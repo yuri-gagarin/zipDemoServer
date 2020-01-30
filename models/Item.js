5a197021d363e28d78a3f9652fe6d96f91617e8f
Yuriy Ivanov <ymivanov@icloud.com>
2020-01-30 23:56:48 +0200

    Item model

---
+++ models/Item.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+  store: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Store"
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: String,
+    enum: ["sports", "reacreation", "transportation", "childrens", "clothing", "general"],
+    default: "general"
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  pictures: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ItemImage"
+    }
+  ],
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  editedAt: {
+    type: Date,
+    required: true
+  }
+});
+
+export default mongoose.model("Item", itemSchema);
